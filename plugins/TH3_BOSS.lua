df8742a8de765212822dad66faeb7d720d4cca27
alzaen <ahmed7879500@gmail.com>
2016-06-29 03:37:02 +0300

    Delete TH3_BOSS.lua

--- plugins/TH3_BOSS.lua
+++
@@ -1,27 +0,0 @@
-do 
-
-local function run(msg, matches) 
-
-if ( msg.text ) then
-
-  if ( msg.to.type == "user" ) then
-
-     return "للتحدث مع المطور اضغط على المعرف التالي \n  @oOMOOMOo \n او اذا محظور اضغط هنا \n  @DEVIRAQ_BOT\n قنأة البوت \n  @oOBOBOo 🔶  "
-     
-  end 
-   
-end 
-
--- OMOOMOo 
-
-end 
-
-return { 
-  patterns = { 
-       "(.*)$"
-  }, 
-  run = run, 
-} 
-
-end 
--- By @SAJJADNOORI
