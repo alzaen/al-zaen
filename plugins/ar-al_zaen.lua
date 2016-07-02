3dfc6e0d17276f764aabe9a216a29cf810df172d
alzaen <ahmed7879500@gmail.com>
2016-07-02 03:18:21 +0300

    Add files via upload

---
+++ plugins/ar-al_zaen.lua
@@ -0,0 +1,24 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY ahmed_al_zaen                   ▀▄ ▄▀ 
+▀▄ ▄▀     BY ahmed_al_zaen (@ahmed_al_zaen)    ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY ahmed_al_zaen          ▀▄ ▄▀   
+▀▄ ▄▀          Dev  : المطور               ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+
+do
+
+function run(msg, matches)
+  return '❣@ahmed_al_zaen.git \n❣   #المطور @ahmed_al_zaen \n'
+end
+
+return {
+  patterns = {
+    "^اصدار"
+  }, 
+  run = run 
+}
+
+end
